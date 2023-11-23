0ed07c9e0880fa0e74d58025eed893793e088530
sooakim <alice2863@naver.com>
2023-11-24 01:16:43 +0900

    Initial Commit

---
+++ ChunsikDorak/ChunsikDorakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChunsikDorakApp.swift
+//  ChunsikDorak
+//
+//  Created by 김수아 on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ChunsikDorakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
